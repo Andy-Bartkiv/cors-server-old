61213e84e590b052c2757f1f1e65ac36a35c1a11
unknown <andy.bartkiv@gmail.com>
2023-04-21 23:13:49 -0400

    update

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
+    // requireHeader: [],
     removeHeaders: ['cookie', 'cookie2']
+    // removeHeaders: []
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
